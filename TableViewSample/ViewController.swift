b1cf2010f8b6bdb3b7107fde727aff076b1c79e9
nwalde <nwalde@monitechnologies.com>
2017-04-10 15:48:30 +0530

    Initial Commit

---
+++ TableViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewSample
+//
+//  Created by Nikhilesh Walde on 10/04/2017.
+//  Copyright © 2017 Prepay Nation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
