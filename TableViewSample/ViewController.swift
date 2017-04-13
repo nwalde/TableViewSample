107757f52a23559079ebac51979994c3cb26250a
nwalde <nwalde@monitechnologies.com>
2017-04-13 19:12:12 +0530

    sample
    sample

--- TableViewSample/ViewController.swift
+++ TableViewSample/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var sampleTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sampleTableView.layer.cornerRadius = 10
+        self.sampleTableView.layer.masksToBounds = true
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        //        cell.ContainerView.layer.backgroundColor = UIColor.red.cgColor
+        
+        return cell
+    }
+
+    
 
 }
 
